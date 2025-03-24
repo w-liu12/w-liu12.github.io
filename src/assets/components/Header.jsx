9763c4df5bb69b62b8eb18e84baedc216260815d
Wendy Liu <wendy_liu@berkeley.edu>
2025-03-24 15:38:59 -0400

    Updated resume link

--- src/assets/components/Header.jsx
+++ src/assets/components/Header.jsx
@@ -14,7 +14,7 @@ function Header() {
           <img className="header-logo" src={wendyPurple} />
         </a>
         <HashLink smooth to="/home#projects">Projects</HashLink>
-        <a href="https://wendy-liu-resume.tiiny.site" target="_blank">
+        <a href="https://www.figma.com/proto/AurGrmZy8NN7bz7RCkyfh1/Kai-Wen-Liu-Resume?page-id=124%3A2&node-id=124-11&viewport=225%2C287%2C0.67&t=XkDDYGPf7xEwdO0M-1&scaling=min-zoom&content-scaling=fixed" target="_blank">
           Resume
         </a>
       </nav>
